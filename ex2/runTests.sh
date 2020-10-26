04355427eb140630ee9e133bd69797d503abfe2f
Tomás <tomasphilippart@gmail.com>
2020-10-26 01:56:52 +0000

    Minor fix on runTests.sh script

--- ex2/runTests.sh
+++ ex2/runTests.sh
@@ -4,7 +4,6 @@ inputdir=$1
 outputdir=$2
 maxthreads=$3
 
-make -i -f Makefile > /dev/null 2>&1 # make and ignore any outputs
 mkdir -p $outputdir # if outputdir doesnt exist, create it. ignore errors if it already exists
 export GREP_COLOR='00;38;5;157'
 
