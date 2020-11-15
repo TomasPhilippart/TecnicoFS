9f3e4aebb419a524eb99ceff0d74616f6247ecea
Tomás <tomasphilippart@gmail.com>
2020-11-15 21:44:18 +0000

    Updated script to check for integer in maxthreads

--- ex2/runTests.sh
+++ ex2/runTests.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# arguments
 inputdir=$1
 outputdir=$2
 maxthreads=$3
 
-# check arguments
+# check if inputdir and outputdir exist
 [ ! -d "$inputdir" ] && { echo "Directory $inputdir doesn't exist."; exit 1; }
 [ ! -d "$outputdir" ] && { echo "Directory $outputdir doesn't exist."; exit 1; }
+
+# check if maxthreads is an integer
+if ! [[ $maxthreads =~ ^[0-9]+$ ]]; then
+   echo "Maxthreads must be an integer." >&2; exit 1
+fi
+
+# check if maxthreads is greater than 0
 [ ! $maxthreads -gt 0 ] && { echo "Maxthreads must be greater than 0."; exit 1; }
 
 export GREP_COLOR='00;38;5;157'
